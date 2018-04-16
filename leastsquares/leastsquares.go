3ce6f4479ba0d2eb31f5399e5e9fe57a24ddbfef
liwei02 <liwei02@qiniu.com>
2018-04-16 16:11:57 +0800

    add newSlice newSlice2

--- leastsquares/leastsquares.go
+++ leastsquares/leastsquares.go
@@ -17,6 +17,34 @@ func New(points []base.Pointer) (base.CurveFittinger, error) {
 	if len(points) < 2 {
 		return nil, errors.New("point num must gte 2")
 	}
+
+	p := make([][2]float64, len(points))
+	for _, v := range points {
+		p = append(p, [2]float64{v.X(), v.Y()})
+	}
+
+	ls := &LS{}
+	ls.init(p)
+	return ls, nil
+}
+
+// NewSlice allocate and init a LS with the given slice points
+func NewSlice(points []float64) (base.CurveFittinger, error) {
+	if len(points) < 2 {
+		return nil, errors.New("point num must gte 2")
+	}
+
+	ls := &LS{}
+	ls.initWithSlice(points)
+	return ls, nil
+}
+
+// NewSlice2 same as NewSlice
+func NewSlice2(points [][2]float64) (base.CurveFittinger, error) {
+	if len(points) < 2 {
+		return nil, errors.New("point num must gte 2")
+	}
+
 	ls := &LS{}
 	ls.init(points)
 	return ls, nil
@@ -27,20 +55,32 @@ func (ls *LS) GetY(x float64) (y float64) {
 	return ls.b1*x + ls.b0
 }
 
-func (ls *LS) init(points []base.Pointer) {
+func (ls *LS) init(points [][2]float64) {
 	var n, sumX, sumY, sumX2, sumXY float64
-
 	n = float64(len(points))
-
 	for i := 0; i < len(points); i++ {
-		sumX += points[i].X()
-		sumY += points[i].Y()
-		sumX2 += points[i].X() * points[i].X()
-		sumXY += points[i].X() * points[i].Y()
+		sumX += points[i][0]
+		sumY += points[i][1]
+		sumX2 += points[i][0] * points[i][0]
+		sumXY += points[i][0] * points[i][1]
 	}
+	ls.genParams(n, sumX, sumY, sumX2, sumXY)
+}
 
-	println(n, sumX, sumY, sumX2, sumXY)
+func (ls *LS) initWithSlice(points []float64) {
+	var n, sumX, sumY, sumX2, sumXY float64
+	n = float64(len(points))
+	for i, y := range points {
+		x := float64(i)
+		sumX += x
+		sumY += y
+		sumX2 += x * x
+		sumXY += x * y
+	}
+	ls.genParams(n, sumX, sumY, sumX2, sumXY)
+}
 
+func (ls *LS) genParams(n, sumX, sumY, sumX2, sumXY float64) {
 	ls.b1 = (sumXY*n - sumX*sumY) / (sumX2*n - sumX*sumX)
 	ls.b0 = sumY/n - ls.b1*sumX/n
 }
--- leastsquares/leastsquares_test.go
+++ leastsquares/leastsquares_test.go
@@ -29,3 +29,51 @@ func TestLS(t *testing.T) {
 	t.Log(4, ls.GetY(4))
 	t.Log(5, ls.GetY(5))
 }
+
+func TestLSSlice(t *testing.T) {
+	points := []float64{0}
+	ls, err := NewSlice(points)
+	if err == nil || ls != nil {
+		t.Fatal()
+	}
+
+	points = append(points, 1, 2)
+	ls, err = NewSlice(points)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("ls: %#v", ls)
+
+	if ls.GetY(-1) != -1 {
+		t.Fatal()
+	}
+
+	t.Log(3, ls.GetY(3))
+	t.Log(4, ls.GetY(4))
+	t.Log(5, ls.GetY(5))
+}
+
+func TestLSSlice2(t *testing.T) {
+	points := [][2]float64{[2]float64{1, 1}}
+	ls, err := NewSlice2(points)
+	if err == nil || ls != nil {
+		t.Fatal()
+	}
+
+	points = append(points, [2]float64{2, 2})
+	ls, err = NewSlice2(points)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("ls: %#v", ls)
+
+	if ls.GetY(0) != 0 {
+		t.Fatal()
+	}
+
+	t.Log(3, ls.GetY(3))
+	t.Log(4, ls.GetY(4))
+	t.Log(5, ls.GetY(5))
+}
